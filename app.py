ff10c4ef3cf5bfb0aaa5d45f3bddd8264ce647cd
Pramukh JS <pramukh660@gmail.com>
2023-06-30 00:21:16 +0530

    nossl

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ config = {
     'dbName': 'yugabyte',
     'dbUser': 'admin',
     'dbPassword': '3aSbVrpXXcxuS3aexM-3yDQA5z3j8-',
-    'sslMode': 'verify-full',
-    'sslRootCert': "/root.crt"
+    'sslMode': '',
+    'sslRootCert': ""
 }
 
 def main(conf):
